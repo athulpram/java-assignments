11bf9c567b772957688d8abdd2765edcae2834b2
praathul <44019979+athulpram@users.noreply.github.com>
2019-03-11 00:35:19 +0530

    Done challenge reverse array

--- src/com/step/assignments/arrays/ArrayReverse.java
+++ src/com/step/assignments/arrays/ArrayReverse.java
@@ -1,7 +1,34 @@
 package com.step.assignments.arrays;
 
+import java.util.Scanner;
+
+import static com.step.assignments.arrays.MinElement.readIntegers;
+import static com.step.assignments.arrays.sort.ArraySort.printArray;
+
 public class ArrayReverse {
-//    public static void main(String[] args) {
-//        new int int  = 5;
-//    }
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int count;
+        System.out.println("Enter the number of elements in array : ");
+        count  = scanner.nextInt();
+
+        System.out.println("Enter "+count+" numbers....");
+        int[] numbers = readIntegers(count);
+
+        System.out.println("Array before reverse : ");
+        printArray(numbers);
+
+        reverseArray(numbers);
+
+        System.out.println("Array after reverse");
+        printArray(numbers);
+    }
+
+    private static void reverseArray(int[] numbers) {
+        for (int index = 0; index < numbers.length/2; index++) {
+            int temp = numbers[index];
+            numbers[index] = numbers[numbers.length-(index+1)];
+            numbers[numbers.length-(index+1)] = temp;
+        }
+    }
 }
